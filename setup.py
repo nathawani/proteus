d4e9fae3dadbcd4799a7d201b0d91e8d35335faa
cekees <613654+cekees@users.noreply.github.com>
2024-09-20 13:59:40 -0500

    convert back to development branch after 1.8.3 release

--- setup.py
+++ setup.py
@@ -797,7 +797,7 @@ EXTENSIONS_TO_BUILD = [
 
 def setup_given_extensions(extensions):
     setup(name='proteus',
-          version='1.8.3',
+          version='1.8.3.dev',
           classifiers=[
               'Development Status :: 4 - Beta',
               'Environment :: Console',
