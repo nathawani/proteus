dfbad41257690e9eee825418788ade814f82f0cc
Darsh Nathawani <nathawanidarsh@gmail.com>
2024-10-31 16:50:01 -0500

    Set element size differently

--- test/ci/poisson_3d_tetgen_p.py
+++ test/ci/poisson_3d_tetgen_p.py
@@ -45,7 +45,7 @@ hull_center = (0.5*hull_length,
 
 nLevels = 1
 
-he = L[0]/10.0 * 0.5**opts.Refinement
+he =  L[0] / ( ( 20 * 1.26**opts.Refinement) )
 #he = hull_draft/1.0
 #he = hull_draft/6.0
 genMesh=opts.genMesh
